8007fc534a2dca1ec37bef46c00b0b6f8c22bbd7
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2025-08-21 20:10:44 +0100

    Counter app using MVVM

--- CounterMVVM/app/src/main/java/tips/my2cents/countermvvm/MainActivity.kt
+++ CounterMVVM/app/src/main/java/tips/my2cents/countermvvm/MainActivity.kt
@@ -1,10 +1,10 @@
 package tips.my2cents.countermvvm
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,10 +16,8 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Modifier
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -30,9 +28,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel: CounterViewModel by viewModels()
             CounterMVVMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TheCounterApp(modifier = Modifier.padding(innerPadding))
+                    TheCounterApp(
+                        modifier = Modifier.padding(innerPadding),
+                        viewModel = viewModel
+                    )
                 }
             }
         }
@@ -40,16 +42,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TheCounterApp(modifier: Modifier = Modifier) {
-    val count = remember { mutableStateOf(0) }
-
-    fun increment() {
-        count.value++
-    }
-
-    fun decrement() {
-        count.value--
-    }
+fun TheCounterApp(modifier: Modifier = Modifier, viewModel: CounterViewModel) {
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -57,16 +50,16 @@ fun TheCounterApp(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Count: ${count.value}",
+            text = "Count: ${viewModel.count.value}",
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(16.dp))
         Row {
-            Button(onClick = { increment() }) {
+            Button(onClick = { viewModel.increment() }) {
                 Text("Increment")
             }
-            Button(onClick = { decrement() }) {
+            Button(onClick = { viewModel.decrement() }) {
                 Text("Decrement")
             }
         }
